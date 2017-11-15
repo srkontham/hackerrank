53a2cc7e6e1c19c2a733df5d80d5a053b8e11e8e
sreddyk <sreddyk@DIN67001025.corp.capgemini.com>
2017-11-15 18:53:05 +0530

    Added solution for varargs solution in java

---
+++ src/org/srk/solutions/java/advanced/varargsAdd/VarargsAddition.java
@@ -0,0 +1,59 @@
+package org.srk.solutions.java.advanced.varargsAdd;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+public class VarargsAddition {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Enter Input:");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int n1 = Integer.parseInt(br.readLine());
+			int n2 = Integer.parseInt(br.readLine());
+			int n3 = Integer.parseInt(br.readLine());
+			int n4 = Integer.parseInt(br.readLine());
+			int n5 = Integer.parseInt(br.readLine());
+			int n6 = Integer.parseInt(br.readLine());
+			Add ob = new Add();
+			ob.add(n1, n2);
+			ob.add(n1, n2, n3);
+			ob.add(n1, n2, n3, n4, n5);
+			ob.add(n1, n2, n3, n4, n5, n6);
+			Method[] methods = Add.class.getDeclaredMethods();
+			Set<String> set = new HashSet<>();
+			boolean overload = false;
+			for (int i = 0; i < methods.length; i++) {
+				if (set.contains(methods[i].getName())) {
+					overload = true;
+					break;
+				}
+				set.add(methods[i].getName());
+
+			}
+			if (overload) {
+				throw new Exception("Overloading not allowed");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class Add {
+	void add(int... nums) {
+		int sum = 0, i = 0;
+		for (int n : nums) {
+			sum = sum + n;
+			if (i++ != nums.length - 1)
+				System.out.print(n + "+");
+			else
+				System.out.print(n + "=");
+		}
+
+		System.out.println(sum);
+	}
+}
